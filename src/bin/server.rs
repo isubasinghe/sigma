5445741acb24f8cd0620f1953c969c46231a49f8
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-12-13 01:07:45 +0100

    reading request

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,9 @@
 use capnp::serialize_packed;
 use sigma::shared::server::*;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use std::i8::MAX;
 use std::net::ToSocketAddrs;
 use std::time::Duration;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 use opentelemetry::sdk::export::trace::stdout;
 use opentelemetry::sdk::trace::{self, Sampler};
@@ -10,13 +11,15 @@ use tracing::{error, info, info_span, warn};
 
 use opentelemetry::sdk::Resource;
 use opentelemetry::KeyValue;
+use std::io;
+use tokio::net::TcpListener;
+use tokio::net::UdpSocket;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::Registry;
-use tokio::net::UdpSocket;
-use tokio::net::TcpListener;
-use std::io;
+use sigma::shared::proto_capnp::*;
 
 const PORT: u16 = 3001;
+const MAX_RETRIES: u64 = 10;
 
 fn handle_client() {}
 #[tokio::main]
@@ -36,7 +39,6 @@ async fn main() {
         .install_simple()
         .unwrap(); // stdout::new_pipeline().install_simple();
 
-
     // Create a tracing layer with the configured tracer
     let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
 
@@ -44,34 +46,85 @@ async fn main() {
     // that impls `LookupSpan`
     let subscriber = Registry::default().with(telemetry);
     tracing::subscriber::set_global_default(subscriber).unwrap();
-    info_span!("data","TraceId" = "a2e4f3r12i83rc21", "Starting on port {}", PORT);
-
+    info_span!(
+        "data",
+        "TraceId" = "a2e4f3r12i83rc21",
+        "Starting on port {}",
+        PORT
+    );
 
     let addr = ("0.0.0.0", PORT).to_socket_addrs().unwrap().next().unwrap();
 
     let listener = match TcpListener::bind(addr).await {
-        Ok(l) => l, 
-        Err(e) => return
+        Ok(l) => l,
+        Err(e) => return,
     };
 
     loop {
-        let (mut sock,_) = match listener.accept().await {
-            Ok(v) => v, 
-            Err(e) => continue
+        let (mut sock, _) = match listener.accept().await {
+            Ok(v) => v,
+            Err(e) => continue,
         };
         tokio::spawn(async move {
+            let mut retries: u64 = 0;
+
             loop {
                 let sz = match sock.read_u16().await {
-                    Ok(sz) => sz, 
-                    Err(e) => continue
+                    Ok(sz) => sz,
+                    Err(e) => {
+                        if retries == MAX_RETRIES {
+                            return;
+                        }
+                        continue;
+                    }
                 };
                 let sz = usize::from(sz);
                 let mut buf: Vec<u8> = Vec::with_capacity(sz);
                 let data = match sock.read_exact(&mut buf).await {
-                    Ok(data) => data, 
-                    Err(_) => continue
+                    Ok(data) => data,
+                    Err(e) => {
+                        if retries == MAX_RETRIES {
+                            return;
+                        }
+                        continue;
+                    }
                 };
-            }   
+                let buf = std::io::BufReader::new(buf.as_slice());
+                let message_reader = match serialize_packed::read_message(buf, ::capnp::message::ReaderOptions::new()) {
+                    Ok(msg_reader) => msg_reader, 
+                    Err(e) => {
+                        if retries == MAX_RETRIES {
+                            return;
+                        }
+                        continue;
+                    }
+                };
+                let request = match message_reader.get_root::<request::Reader>() {
+                    Ok(r) => r, 
+                    Err(e) => {
+                        if retries == MAX_RETRIES {
+                            return;
+                        }
+                        continue;
+                    }
+                };
+                let request = match request.which() {
+                    Ok(r) => r, 
+                    Err(e) => {
+                        if retries == MAX_RETRIES {
+                            return;
+                        }
+                        continue;
+                    }
+                };
+                match request {
+                    request::Which::RegisterClient(c) => {
+                    }
+                    request::Which::MakeLock(l) => {}
+                }
+                
+                retries = 0;
+            }
         });
     }
 }
--- src/shared/server.rs
+++ src/shared/server.rs
@@ -1,28 +1,30 @@
 use crate::shared::proto_capnp::pair;
 use arc_swap::ArcSwapOption;
-use tokio::sync::Mutex;
-use tokio::sync::mpsc;
-use tokio::sync::RwLock;
 use dashmap::DashSet;
+use tokio::sync::mpsc;
+use tokio::sync::Mutex;
+use tokio::sync::RwLock;
 
 #[derive(Hash)]
-pub struct Id (u64, u64);
+pub struct Id(u64, u64);
 
 impl PartialEq for Id {
     fn eq(&self, other: &Self) -> bool {
-       self.1 == other.1 && self.0 == other.0
+        self.1 == other.1 && self.0 == other.0
     }
 }
 
 impl Eq for Id {}
 
-struct Tombstone {  
-    entries: DashSet<Id>
+struct Tombstone {
+    entries: DashSet<Id>,
 }
 
 impl Tombstone {
     pub fn new() -> Tombstone {
-        Tombstone { entries: DashSet::new() }
+        Tombstone {
+            entries: DashSet::new(),
+        }
     }
 }
 
@@ -36,7 +38,7 @@ type Owner = (u64, u64);
 
 pub struct Server<T> {
     receiver: mpsc::Receiver<T>,
-    owner: Mutex<Option<Owner>>
+    owner: Mutex<Option<Owner>>,
 }
 
 const SERVER_BUFFER_SIZE: usize = 2048;
@@ -48,7 +50,7 @@ impl<T> Server<T> {
         (
             Server {
                 receiver: rx,
-                owner: Mutex::new(None), 
+                owner: Mutex::new(None),
             },
             tx,
         )
@@ -60,8 +62,8 @@ impl<T> Server<T> {
 
     pub async fn try_lock(&mut self, id: Id) {
         match self.owner.lock().await.as_mut() {
-            None => {}, 
-            Some(owner) => {},
+            None => {}
+            Some(owner) => {}
         }
     }
 
